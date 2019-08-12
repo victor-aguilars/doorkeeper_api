72f96a519ee84978c4d60e685b9a98483fe5b996
victor-aguilars <victor.aguilarsnz@gmail.com>
2019-08-12 18:39:40 -0500

    Create items with api

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -6,5 +6,24 @@ class Api::ItemsController < Api::BaseController
     respond_with @items
   end
 
+  # POST /items
+  # POST /items.json
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      respond_with @item
+    else
+      respond_with @item.errors
+    end
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+
   # the rest of actions
 end
